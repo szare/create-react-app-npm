8e71f614c99d8fd00d28c4b9a5ae18dbb0a8589d
Salman Zare <szz@mg-syd-d0056.milestone.corp>
2017-12-06 14:26:52 +1100

    initial

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import InputNumber from 'components/InputNumber';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+       <InputNumber/>
+      </div>
+    );
+  }
+}
+
+export default App;
